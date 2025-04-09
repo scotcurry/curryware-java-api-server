af0c6aeaaec9ca3a4c08228750fd62393f131063
Scot Curry <scotcurry4@gmail.com>
2025-04-09 19:15:44 -0400

    Moved to JSON logging.

--- src/main/java/org/curryware/controllers/PlayerController.java
+++ src/main/java/org/curryware/controllers/PlayerController.java
@@ -1,5 +1,8 @@
 package org.curryware.controllers;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.curryware.currywarejavaapiserver.CurrywareJavaApiServerApplication;
 import org.curryware.playerservice.repositories.PlayerRecord;
 import org.curryware.playerservice.repositories.PlayerService;
 import org.springframework.web.bind.annotation.*;
@@ -7,6 +10,8 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("players")
 public class PlayerController {
+
+    private static final Logger logger = LogManager.getLogger(PlayerController.class);
     private final PlayerService playerService;
     
     public PlayerController(PlayerService playerService) {
@@ -15,6 +20,7 @@ public class PlayerController {
     
     @GetMapping("/getplayers")
     public @ResponseBody Iterable<PlayerRecord> get() {
+        logger.debug("getplayers called");
         return playerService.findAll();
     }
 
--- src/main/java/org/curryware/currywarejavaapiserver/CurrywareJavaApiServerApplication.java
+++ src/main/java/org/curryware/currywarejavaapiserver/CurrywareJavaApiServerApplication.java
@@ -1,5 +1,7 @@
 package org.curryware.currywarejavaapiserver;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,8 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 public class CurrywareJavaApiServerApplication {
 
+    private static final Logger logger = LogManager.getLogger(CurrywareJavaApiServerApplication.class);
     public static void main(String[] args) {
 
         SpringApplication.run(CurrywareJavaApiServerApplication.class, args);
+        logger.debug("Launching the CurrywareJavaApiServerApplication");
     }
 }
