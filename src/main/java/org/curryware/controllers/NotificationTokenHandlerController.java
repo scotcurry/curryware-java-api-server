c899b09e112a785ed0a9652f63ccbe7a5ededa1d
Scot Curry <scotcurry4@gmail.com>
2026-09-08 15:38:54 -0400

    Changed to a POST for NotificationTokenHandlerController and updated.

--- src/main/java/org/curryware/controllers/NotificationTokenHandlerController.java
+++ src/main/java/org/curryware/controllers/NotificationTokenHandlerController.java
@@ -5,9 +5,9 @@ import org.apache.logging.log4j.Logger;
 import org.curryware.userservice.UserInfoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,12 +21,24 @@ public class NotificationTokenHandlerController {
         this.userInfoService = userInfoService;
     }
 
-    @GetMapping("/storeToken")
-    public ResponseEntity<Void> handleNotificationToken(@RequestParam("deviceToken") String deviceToken) {
-        logger.debug("handleNotificationToken called with deviceToken={}", deviceToken);
-        boolean success = userInfoService.saveApnsToken(deviceToken);
+    @PostMapping("/storeToken")
+    public ResponseEntity<Void> handleNotificationToken(@RequestBody DeviceTokenRequest request) {
+        logger.debug("handleNotificationToken called with deviceToken={}", request.getDeviceToken());
+        boolean success = userInfoService.saveApnsToken(request.getDeviceToken());
         return success
                 ? ResponseEntity.status(HttpStatus.OK).build()
                 : ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    public static class DeviceTokenRequest {
+        private String deviceToken;
+
+        public String getDeviceToken() {
+            return deviceToken;
+        }
+
+        public void setDeviceToken(String deviceToken) {
+            this.deviceToken = deviceToken;
+        }
     }
 }
